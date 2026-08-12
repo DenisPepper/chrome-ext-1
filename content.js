6c3f2eecc098d98ffcc98197be4d55a4c181cbf3
DenisPepper <thech@yandex.ru>
2026-08-12 11:41:58 +0300

    Добавит декоратор для проверки вкладок

--- content.js
+++ content.js
@@ -1,20 +1,24 @@
 // в content.js (который работает прямо на странице сайта) стандартные ESM-импорты через манифест до сих пор официально
 // не поддерживаются из коробки, там логика подключения модулей немного сложнее.
 
-const useCopyButtonHandler = (es) => {
-  window.addEventListener(es.TRIGGER_TITLE_CHECK, () => {
+function addTabChecking(fn) {
+  return function (...args) {
     // Игнорируем, если вкладка не активна или не видна
     if (document.hidden) return;
-
     // Игнорируем iframe (если нужно)
     if (window !== window.top) return;
+    fn.apply(this, args);
+  };
+}
 
+const useCopyButtonHandler = (es) => {
+  const fn = () => {
     const titleTag = document.querySelector("head title");
     const msg = titleTag ? titleTag.textContent : "no title on this page";
-
-    // Отправляем текст обратно в расширение (его поймает sidepanel.js)
     chrome.runtime.sendMessage({ action: es.RETURN_PAGE_TITLE, data: msg });
-  });
+  }
+  const handler = addTabChecking(fn)
+  window.addEventListener(es.TRIGGER_TITLE_CHECK, handler);
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
