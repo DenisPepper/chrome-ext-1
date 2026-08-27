eba55da9ca6eea30ee364733fe74674d65a4097c
DenisPepper <thech@yandex.ru>
2026-08-27 12:49:42 +0300

    Добавит запрос данных с url

--- ext/worker/actions/copy-page-data.js
+++ ext/worker/actions/copy-page-data.js
@@ -1,9 +1,17 @@
 import { injectScript, injectFunction } from '../util/scripting.js';
 
-function getUrlWithPrefix(prefix) {
-    // Этот код выполняется в контексте страницы.
-    // Аргументы передаются через массив args в executeScript.
-    return `${prefix}: ${window.location.href}`;
+// Этот код выполняется в контексте страницы.
+// Аргументы передаются через массив args в executeScript.
+async function getUrlWithPrefix(prefix) {
+    try {
+        const res = await fetch('https://cad.stilkuhni.com/ecadweb/BaseElements/ViewElementLimits?type=1&t=1732&e=22224', {
+            credentials: "include",
+        });
+        const text = await res.text();
+        return `${prefix}: ${window.location.href} | ${text.slice(0, 10)}`;
+    } catch (error) {
+        return null;
+    }
 }
 
 export async function copyPageData(payload) {
